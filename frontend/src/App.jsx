5a7a6fbe945041d46b20f95f635e4cc29afb8f87
batman22827 <52332286+batman22827@users.noreply.github.com>
2024-03-06 13:27:24 +0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 
 import { Cabinet } from './pages/cabinet'
+import { Reg } from './pages/reg'
 
 import { Login } from './pages/login'
 
@@ -22,6 +23,7 @@ function App() {
     <div className="App">
     <Routes>
           <Route path="/login/"element={<Login/>}/>
+          <Route path="/register/"element={<Reg/>}/>
           <Route path="/cabinet/"element={<Cabinet/>}/>
         </Routes>
         </div>
